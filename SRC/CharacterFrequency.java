cdc008bf3a81559114cdd312733f7e6fc5fdf27b
maligaullas-ship-it <maligaullas@gmail.com>
2026-05-04 22:14:04 +0530

    coded to console

---
+++ SRC/CharacterFrequency.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class CharacterFrequency {
+
+    public static String[][] getFrequency(String str) {
+        int[] freq = new int[256];
+
+        // Count frequency
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        // Count unique characters
+        int uniqueCount = 0;
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0) uniqueCount++;
+        }
+
+        // Store result
+        String[][] result = new String[uniqueCount][2];
+        int index = 0;
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0) {
+                result[index][0] = String.valueOf((char) i);
+                result[index][1] = String.valueOf(freq[i]);
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String[][] data = getFrequency(input);
+
+        System.out.println("\nCharacter\tFrequency");
+        System.out.println("-------------------------");
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i][0] + "\t\t" + data[i][1]);
+        }
+    }
+}
